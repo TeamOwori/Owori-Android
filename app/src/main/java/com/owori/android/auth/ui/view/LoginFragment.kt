7d283504e2da12d98c8463454725b62eeb2b67f8
민대인 <bamin0422@gmail.com>
2023-10-05 06:17:47 +0900

    feat: 구글 로그인 기능 구현 - 테스팅 #12

--- app/src/main/java/com/owori/android/auth/ui/view/LoginFragment.kt
+++ app/src/main/java/com/owori/android/auth/ui/view/LoginFragment.kt
@@ -1,12 +1,20 @@
 package com.owori.android.auth.ui.view
 
+import android.content.ContentValues.TAG
+import android.util.Log
 import androidx.activity.addCallback
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.viewModels
-import com.owori.android.auth.ui.viewmodel.LoginViewModel
-import com.owori.android.common.ui.view.BaseFragment
+import com.google.android.gms.auth.api.identity.GetSignInIntentRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.common.api.ApiException
 import com.owori.android.R
-import com.owori.android.databinding.FragmentLoginBinding
+import com.owori.android.auth.ui.viewmodel.LoginViewModel
 import com.owori.android.common.navigateTo
+import com.owori.android.common.ui.view.BaseFragment
+import com.owori.android.databinding.FragmentLoginBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -31,8 +39,40 @@ class LoginFragment : BaseFragment<FragmentLoginBinding, LoginViewModel>(R.layou
             }
 
             callGoogleLogin.observe(viewLifecycleOwner) {
-                navigateTo(R.id.action_loginFragment_to_PolicyFragment)
+                val request = GetSignInIntentRequest.builder()
+                    .setServerClientId(getString(R.string.firebase_web_client_id))
+                    .build()
+
+                Identity.getSignInClient(requireActivity())
+                    .getSignInIntent(request)
+                    .addOnSuccessListener { result ->
+                        val intentSenderRequest = IntentSenderRequest.Builder(result).build()
+                        resultLauncher.launch(intentSenderRequest)
+                        Log.d(TAG, "GoogleLogin launched...")
+                    }.addOnFailureListener { e ->
+                        Log.d(TAG, "preparing intent failed: ${e.message}")
+                    }
             }
+        }
+    }
+
+    private val resultLauncher = registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) {
+            result: ActivityResult ->
+        try {
+            val credential = Identity.getSignInClient(requireActivity()).getSignInCredentialFromIntent(result.data)
+            val idToken = credential.googleIdToken
+            when {
+                idToken != null -> {
+                    // Got an ID token from Google. Use it to authenticate
+                    Log.d(TAG, "Got ID token. $idToken")
+                    navigateTo(R.id.action_loginFragment_to_PolicyFragment)
+                }
+                else -> {
+                    Log.d(TAG, "No ID token!")
+                }
+            }
+        } catch (e: ApiException) {
+            Log.d(TAG, "google login failed : $e")
         }
     }
 
@@ -41,4 +81,6 @@ class LoginFragment : BaseFragment<FragmentLoginBinding, LoginViewModel>(R.layou
             requireActivity().finish()
         }
     }
+
+
 }
--- app/src/main/java/com/owori/android/auth/ui/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/owori/android/auth/ui/viewmodel/LoginViewModel.kt
@@ -1,6 +1,9 @@
 package com.owori.android.auth.ui.viewmodel
 
 import androidx.lifecycle.LiveData
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.owori.android.common.SingleLiveEvent
 import com.owori.android.common.ui.viewmodel.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -12,12 +15,13 @@ class LoginViewModel @Inject constructor() : BaseViewModel() {
     val callKakaoLogin: LiveData<Unit> = _callKakaoLogin
     private val _callGoogleLogin: SingleLiveEvent<Unit> = SingleLiveEvent()
     val callGoogleLogin: LiveData<Unit> = _callGoogleLogin
+    val auth: FirebaseAuth by lazy { Firebase.auth }
 
     fun onClickKakaoLogin() {
         _callKakaoLogin.call()
     }
 
     fun onClickGoogleLogin() {
-        _callGoogleLogin.call()
+        auth.currentUser?.let {  } ?: _callGoogleLogin.call()
     }
 }
