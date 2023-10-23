8de06d59023f0cd8d1983e04969239b36d0796e0
posite <su84053@gmail.com>
2023-10-23 20:03:27 +0900

    feat: 사용자의 생년월일 입력을 위한 화면 구성 #16

--- app/src/main/java/com/owori/android/auth/ui/viewmodel/BirthDateViewModel.kt
+++ app/src/main/java/com/owori/android/auth/ui/viewmodel/BirthDateViewModel.kt
@@ -1,6 +1,7 @@
 package com.owori.android.auth.ui.viewmodel
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.owori.android.common.SingleLiveEvent
 import com.owori.android.common.ui.viewmodel.BaseViewModel
 import javax.inject.Inject
@@ -8,8 +9,17 @@ import javax.inject.Inject
 class BirthDateViewModel @Inject constructor() : BaseViewModel() {
     private val _btnNext: SingleLiveEvent<Unit> = SingleLiveEvent()
     val btnNext: LiveData<Unit> = _btnNext
+    private val _btnBack: SingleLiveEvent<Unit> = SingleLiveEvent()
+    val btnBack: LiveData<Unit> = _btnBack
+
+    val _birthDate: MutableLiveData<String> = MutableLiveData("")
+    val birthDate: LiveData<String> = _birthDate
 
     fun onClickCheckButton() {
         _btnNext.call()
+    }
+
+    fun onClickBackButton() {
+        _btnBack.call()
     }
 }
