47b02ddf60fad538e78e46293f8beb7b89be3c25
민대인 <bamin0422@gmail.com>
2023-10-24 23:38:37 +0900

    feat: ViewPager 스크롤과 유사한 형태로 아이템 노출되도록 ScrollListener 추가 #10

--- app/src/main/java/com/owori/android/presenter/main/home/HomeFragment.kt
+++ app/src/main/java/com/owori/android/presenter/main/home/HomeFragment.kt
@@ -1,18 +1,33 @@
 package com.owori.android.presenter.main.home
 
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.PagerSnapHelper
 import com.owori.android.R
 import com.owori.android.core.BaseFragment
 import com.owori.android.databinding.FragmentHomeBinding
 import com.owori.android.presenter.main.home.adapter.DdayAdapter
 import com.owori.android.presenter.main.home.adapter.FamilyMemberAdapter
+import com.owori.android.presenter.util.SnapPagerScrollListener
+import com.owori.android.presenter.util.SnapPagerScrollListener.Companion.ON_SCROLL
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>(R.layout.fragment_home) {
     override val viewModel: HomeViewModel by viewModels()
     private val emotionListAdapter: FamilyMemberAdapter by lazy { FamilyMemberAdapter() }
     private val dDayListAdapter: DdayAdapter by lazy { DdayAdapter { id -> viewModel.deleteDdayItem(id) } }
+    private val snapHelper = PagerSnapHelper()
+    private val listener = SnapPagerScrollListener(
+        snapHelper,
+        ON_SCROLL,
+        true,
+        object : SnapPagerScrollListener.OnChangeListener {
+            override fun onSnapped(position: Int) {
+                //position 받아서 이벤트 처리
+            }
+        }
+    )
 
     override fun setBindingVariables() {
         with(binding) {
@@ -34,7 +49,9 @@ class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>(R.layout.f
     override fun initView() {
         with(binding) {
             emotionRecyclerView.adapter = emotionListAdapter
+            snapHelper.attachToRecyclerView(dDayRecyclerView)
             dDayRecyclerView.adapter = dDayListAdapter
+            dDayRecyclerView.addOnScrollListener(listener)
         }
     }
 }
