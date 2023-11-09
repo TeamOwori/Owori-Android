695e4818e7ceeae4361fddf0540cf14f8e50ba7e
민대인 <bamin0422@gmail.com>
2023-11-09 14:39:23 +0900

    feat: 가족사진 RecyclerView 멀티 뷰타입으로 구현 #32

--- app/src/main/java/com/owori/android/presenter/main/home/adapter/FamilyPhotoAdapter.kt
+++ app/src/main/java/com/owori/android/presenter/main/home/adapter/FamilyPhotoAdapter.kt
@@ -1,41 +1,90 @@
 package com.owori.android.presenter.main.home.adapter
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.owori.android.databinding.ItemAddFamilyPhotoBinding
 import com.owori.android.databinding.ItemFamilyPhotoBinding
-import com.owori.android.presenter.model.FamilyPhotoData
+import com.owori.android.presenter.model.FamilyPhotoItem
 
-class FamilyPhotoAdapter : ListAdapter<FamilyPhotoData, FamilyPhotoAdapter.ViewHolder>(FamilyPhotoDiffUtil()) {
+class FamilyPhotoAdapter(private val onClickAddPhoto: () -> Unit = {}) :
+    ListAdapter<FamilyPhotoItem, CommonViewHolder>(FamilyPhotoDiffUtil()) {
 
-    inner class ViewHolder(private val binding: ItemFamilyPhotoBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(familyPhotoData: FamilyPhotoData) {
-            with(binding) {
-                data = familyPhotoData
-            }
-        }
-    }
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
-    ): ViewHolder {
-        val binding = ItemFamilyPhotoBinding.inflate(LayoutInflater.from(parent.context),parent,false)
-        return ViewHolder(binding)
+    ): CommonViewHolder {
+        Log.d("viewType", "$viewType")
+        return when (viewType) {
+            FamilyPhotoItem.FamilyPhotoViewType.PHOTO.ordinal ->
+                CommonViewHolder.FamilyPhotoViewHolder(
+                    ItemFamilyPhotoBinding.inflate(
+                        LayoutInflater.from(
+                            parent.context
+                        ), parent, false
+                    )
+                )
+
+            else -> CommonViewHolder.AddPhotoViewHolder(
+                onClickAddPhoto,
+                ItemAddFamilyPhotoBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+            )
+        }
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun getItemViewType(position: Int): Int =
+        getItem(position).viewType.ordinal
+
+
+    override fun onBindViewHolder(holder: CommonViewHolder, position: Int) {
         holder.bind(getItem(position))
     }
 }
 
-class FamilyPhotoDiffUtil : DiffUtil.ItemCallback<FamilyPhotoData>(){
-    override fun areItemsTheSame(oldItem: FamilyPhotoData, newItem: FamilyPhotoData): Boolean {
-        return oldItem.id == newItem.id
+class FamilyPhotoDiffUtil : DiffUtil.ItemCallback<FamilyPhotoItem>() {
+    override fun areItemsTheSame(oldItem: FamilyPhotoItem, newItem: FamilyPhotoItem): Boolean {
+        return oldItem.viewType == newItem.viewType
     }
 
-    override fun areContentsTheSame(oldItem: FamilyPhotoData, newItem: FamilyPhotoData): Boolean {
+    override fun areContentsTheSame(oldItem: FamilyPhotoItem, newItem: FamilyPhotoItem): Boolean {
         return oldItem.hashCode() == newItem.hashCode()
     }
 }
+
+sealed class CommonViewHolder(
+    binding: ViewDataBinding
+) : RecyclerView.ViewHolder(binding.root) {
+    abstract fun bind(item: FamilyPhotoItem)
+
+    class FamilyPhotoViewHolder(
+        private val binding: ItemFamilyPhotoBinding
+    ) : CommonViewHolder(binding) {
+        override fun bind(item: FamilyPhotoItem) {
+            with(binding) {
+                data = item.photoData
+            }
+        }
+    }
+
+    class AddPhotoViewHolder(
+        private val onClickEvent: () -> Unit = {},
+        private val binding: ItemAddFamilyPhotoBinding
+    ) : CommonViewHolder(binding) {
+        override fun bind(item: FamilyPhotoItem) {
+            with(binding) {
+                addPhotoLayout.setOnClickListener {
+                    onClickEvent()
+                }
+            }
+        }
+    }
+}
+
