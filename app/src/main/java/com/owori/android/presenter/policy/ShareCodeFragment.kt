730c5a4018c254d54803260fa33ed317b812c84c
posite <su84053@gmail.com>
2023-12-17 23:38:11 +0900

    feat: 초대하기 기능 구현 #21

--- app/src/main/java/com/owori/android/presenter/policy/ShareCodeFragment.kt
+++ app/src/main/java/com/owori/android/presenter/policy/ShareCodeFragment.kt
@@ -1,6 +1,11 @@
 package com.owori.android.presenter.policy
 
 
+import android.content.Intent
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import com.owori.android.R
 import com.owori.android.core.BaseFragment
@@ -17,13 +22,28 @@ class ShareCodeFragment : BaseFragment<FragmentShareCodeBinding, ShareCodeViewMo
     }
 
     override fun initView() {
-
+        val builder = SpannableStringBuilder(binding.shareCodeExplain.text.toString())
+        val color9090Span1 = ForegroundColorSpan(ContextCompat.getColor(requireContext(), R.color.grey_909090))
+        val color9090Span2 = ForegroundColorSpan(ContextCompat.getColor(requireContext(), R.color.grey_909090))
+        builder.setSpan(color9090Span1, 12, 19, Spanned.SPAN_INCLUSIVE_INCLUSIVE)
+        builder.setSpan(color9090Span2, 48, 70, Spanned.SPAN_INCLUSIVE_INCLUSIVE)
+        binding.shareCodeExplain.text = builder
     }
 
     override fun initObserver() {
         with(viewModel) {
             btnNext.observe(viewLifecycleOwner) {
                 navigateTo(R.id.action_shareCodeFragment_to_agreeServiceConditionFragment)
+            }
+            btnShare.observe(viewLifecycleOwner) {
+                val intent = Intent(Intent.ACTION_SEND_MULTIPLE).apply {
+                    type = "text/plain"
+                    putExtra(Intent.EXTRA_TEXT, binding.shareCodeText.text.toString())
+                }
+                startActivity(Intent.createChooser(intent, "제목"))
+            }
+            btnBack.observe(viewLifecycleOwner) {
+                requireActivity().onBackPressedDispatcher.onBackPressed()
             }
         }
     }
--- app/src/main/java/com/owori/android/presenter/policy/ShareCodeViewModel.kt
+++ app/src/main/java/com/owori/android/presenter/policy/ShareCodeViewModel.kt
@@ -1,18 +1,74 @@
 package com.owori.android.presenter.policy
 
+import android.os.CountDownTimer
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.owori.android.core.BaseViewModel
 import com.owori.android.presenter.util.SingleLiveEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
-class ShareCodeViewModel @Inject constructor(): BaseViewModel() {
+class ShareCodeViewModel @Inject constructor() : BaseViewModel() {
     private val _btnNext: SingleLiveEvent<Unit> = SingleLiveEvent()
     val btnNext: LiveData<Unit> = _btnNext
+
+    private val _btnShare: SingleLiveEvent<Unit> = SingleLiveEvent()
+    val btnShare: LiveData<Unit> = _btnShare
+
+    private val _btnBack: SingleLiveEvent<Unit> = SingleLiveEvent()
+    val btnBack: LiveData<Unit> = _btnBack
+    private val _customTimerDuration: MutableLiveData<Long> = MutableLiveData(MIllIS_IN_FUTURE)
+    private var oldTimeMills: Long = 0
+    private val _time = MutableLiveData<String>("")
+    val timerJob: Job = viewModelScope.launch(start = CoroutineStart.LAZY) {
+        withContext(Dispatchers.IO) {
+            oldTimeMills = System.currentTimeMillis()
+            while (_customTimerDuration.value!! > 0L) {
+                val delayMills = System.currentTimeMillis() - oldTimeMills
+                if (delayMills == TICK_INTERVAL) {
+                    _customTimerDuration.postValue(_customTimerDuration.value!! - delayMills)
+                    oldTimeMills = System.currentTimeMillis()
+                }
+            }
+        }
+    }
+    private val _timer: CountDownTimer = object : CountDownTimer(MIllIS_IN_FUTURE, TICK_INTERVAL) {
+        override fun onTick(millisUntilFinished: Long) {
+            val minute = millisUntilFinished / 60000L
+            val second = millisUntilFinished % 60000L / 1000L
+            _time.value = "${minute}:${second}"
+            timerJob.start()
+        }
+
+        override fun onFinish() {
+
+        }
+    }.start()
+    val timer: LiveData<String> = _time
+
 
 
     fun onClickCheckButton() {
         _btnNext.call()
+    }
+
+    fun onClickShareButton() {
+        _btnShare.call()
+    }
+
+    fun onClickBackButton() {
+        _btnBack.call()
+    }
+
+    companion object {
+        const val MIllIS_IN_FUTURE = 1800000L
+        const val TICK_INTERVAL = 1000L
     }
 }
