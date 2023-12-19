4150e1e9af15c66dd419610de6ffeb5bda1c4616
민대인 <bamin0422@gmail.com>
2023-12-20 04:54:30 +0900

    fix: dialogFragment resize 로직 개선 #32

--- app/src/main/java/com/owori/android/core/BaseDialogFragment.kt
+++ app/src/main/java/com/owori/android/core/BaseDialogFragment.kt
@@ -1,15 +1,12 @@
 package com.owori.android.core
 
-import android.content.Context
 import android.graphics.Color
-import android.graphics.Point
 import android.graphics.drawable.ColorDrawable
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.WindowManager
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import androidx.fragment.app.DialogFragment
 import com.owori.android.databinding.DialogBaseBinding
 
@@ -25,7 +22,7 @@ class BaseDialogFragment(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = DialogBaseBinding.inflate(inflater, container, false)
         val view = binding.root
         dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
@@ -49,35 +46,11 @@ class BaseDialogFragment(
 
     override fun onResume() {
         super.onResume()
-        requireContext().dialogFragmentResize(this@BaseDialogFragment, dialogX, dialogY)
+        this@BaseDialogFragment.dialog?.window?.setLayout(WRAP_CONTENT, WRAP_CONTENT)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    private fun Context.dialogFragmentResize(dialogFragment: DialogFragment, width: Float, height: Float) {
-        val windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
-        if (Build.VERSION.SDK_INT < 30) {
-            val display = windowManager.defaultDisplay
-            val size = Point()
-            display.getSize(size)
-            val window = dialogFragment.dialog?.window
-            val x = (size.x * width).toInt()
-            val y = (size.y * height).toInt()
-            window?.setLayout(x, y)
-        } else {
-            val rect = windowManager.currentWindowMetrics.bounds
-            val window = dialogFragment.dialog?.window
-            val x = (rect.width() * width).toInt()
-            val y = (rect.height() * height).toInt()
-            window?.setLayout(x, y)
-        }
-    }
-
-    companion object {
-        private const val dialogX = 0.9f
-        private const val dialogY = 0.3f
     }
 }
