fd61926c04e53d8a9209fa5b7274239bb9ec53f1
posite <su84053@gmail.com>
2023-12-22 19:33:46 +0900

    fix: databinding에서 viewbinding으로 수정

--- app/src/main/java/com/owori/android/presenter/policy/FamilyConnectFragment.kt
+++ app/src/main/java/com/owori/android/presenter/policy/FamilyConnectFragment.kt
@@ -1,6 +1,11 @@
 package com.owori.android.presenter.policy
 
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.owori.android.R
 import com.owori.android.core.BaseFragment
@@ -8,26 +13,38 @@ import com.owori.android.core.navigateTo
 import com.owori.android.databinding.FragmentFamilyConnectBinding
 
 
-class FamilyConnectFragment : BaseFragment<FragmentFamilyConnectBinding, FamilyConnectViewModel>(R.layout.fragment_family_connect) {
-    override val viewModel: FamilyConnectViewModel by viewModels()
-    override fun setBindingVariables() {
-        with(binding) {
-            vm = viewModel
+class FamilyConnectFragment : Fragment() {
+    private var _binding: FragmentFamilyConnectBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFamilyConnectBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    private fun initView() {
+        binding.makeCodeButton.setOnClickListener {
+            navigateTo(R.id.action_familyConnectFragment_to_groupFragment)
+        }
+        binding.receivedCodeButton.setOnClickListener {
+            navigateTo(R.id.action_familyConnectFragment_to_inputFamilyCodeFragment)
+        }
+        binding.backButton.setOnClickListener {
+            requireActivity().onBackPressedDispatcher.onBackPressed()
         }
     }
 
-    override fun initView() {
-
-    }
-
-    override fun initObserver() {
-        with(viewModel) {
-            returnLogin.observe(viewLifecycleOwner) {
-                navigateTo(R.id.action_familyConnectFragment_to_inputFamilyCodeFragment)
-            }
-            btnNext.observe(viewLifecycleOwner) {
-                navigateTo(R.id.action_familyConnectFragment_to_groupFragment)
-            }
-        }
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
