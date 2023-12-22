fa66b644a313044d6e7447d05134b9f6b109d6ed
posite <su84053@gmail.com>
2023-12-22 20:04:02 +0900

    fix: 매직 넘버 제거 및  필요 없는 databinding 코드 제거 #21

--- app/src/main/java/com/owori/android/presenter/policy/ShareCodeFragment.kt
+++ app/src/main/java/com/owori/android/presenter/policy/ShareCodeFragment.kt
@@ -22,29 +22,42 @@ class ShareCodeFragment : BaseFragment<FragmentShareCodeBinding, ShareCodeViewMo
     }
 
     override fun initView() {
+        initTextEmphasis()
+        initClickListener()
+    }
+
+    private fun initClickListener() {
+        binding.viewpagerButton.setOnClickListener {
+            navigateTo(R.id.action_shareCodeFragment_to_agreeServiceConditionFragment)
+        }
+        binding.shareCodeButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND_MULTIPLE).apply {
+                type = SHARE_CODE_TYPE
+                putExtra(Intent.EXTRA_TEXT, binding.shareCodeText.text.toString())
+            }
+            startActivity(Intent.createChooser(intent, "아무 제목"))
+        }
+        binding.imgCancel.setOnClickListener {
+            requireActivity().onBackPressedDispatcher.onBackPressed()
+        }
+    }
+
+    private fun initTextEmphasis() {
         val builder = SpannableStringBuilder(binding.shareCodeExplain.text.toString())
         val color9090Span1 = ForegroundColorSpan(ContextCompat.getColor(requireContext(), R.color.grey_909090))
         val color9090Span2 = ForegroundColorSpan(ContextCompat.getColor(requireContext(), R.color.grey_909090))
-        builder.setSpan(color9090Span1, 12, 19, Spanned.SPAN_INCLUSIVE_INCLUSIVE)
-        builder.setSpan(color9090Span2, 48, 70, Spanned.SPAN_INCLUSIVE_INCLUSIVE)
+        builder.setSpan(color9090Span1, FIST_EMPHASIS_PART_START, FIST_EMPHASIS_PART_END, Spanned.SPAN_INCLUSIVE_INCLUSIVE)
+        builder.setSpan(color9090Span2, LAST_EMPHASIS_PART_START, LAST_EMPHASIS_PART_END, Spanned.SPAN_INCLUSIVE_INCLUSIVE)
         binding.shareCodeExplain.text = builder
     }
 
-    override fun initObserver() {
-        with(viewModel) {
-            btnNext.observe(viewLifecycleOwner) {
-                navigateTo(R.id.action_shareCodeFragment_to_agreeServiceConditionFragment)
-            }
-            btnShare.observe(viewLifecycleOwner) {
-                val intent = Intent(Intent.ACTION_SEND_MULTIPLE).apply {
-                    type = "text/plain"
-                    putExtra(Intent.EXTRA_TEXT, binding.shareCodeText.text.toString())
-                }
-                startActivity(Intent.createChooser(intent, "제목"))
-            }
-            btnBack.observe(viewLifecycleOwner) {
-                requireActivity().onBackPressedDispatcher.onBackPressed()
-            }
-        }
+    override fun initObserver() = Unit
+
+    companion object {
+        private const val FIST_EMPHASIS_PART_START = 12
+        private const val FIST_EMPHASIS_PART_END = 19
+        private const val LAST_EMPHASIS_PART_START = 48
+        private const val LAST_EMPHASIS_PART_END =70
+        private const val SHARE_CODE_TYPE = "text/plain"
     }
 }
--- app/src/main/java/com/owori/android/presenter/policy/ShareCodeViewModel.kt
+++ app/src/main/java/com/owori/android/presenter/policy/ShareCodeViewModel.kt
@@ -19,32 +19,23 @@ class ShareCodeViewModel @Inject constructor() : BaseViewModel() {
     init {
         val timer: CountDownTimer = object : CountDownTimer(MIllIS_IN_FUTURE, TICK_INTERVAL) {
             override fun onTick(millisUntilFinished: Long) {
-                val minute = millisUntilFinished / 60000L
-                val second = millisUntilFinished % 60000L / 1000L
+                val minute = millisUntilFinished / MINUTE_UNIT
+                val second = millisUntilFinished % MINUTE_UNIT / SECOND_UNIT
                 _time.value = "${minute}:${second}"
                 timerJob.start()
             }
 
-            override fun onFinish() {
-
-            }
+            override fun onFinish() = Unit
         }.start()
     }
-    private val _btnNext: SingleLiveEvent<Unit> = SingleLiveEvent()
-    val btnNext: LiveData<Unit> = _btnNext
 
-    private val _btnShare: SingleLiveEvent<Unit> = SingleLiveEvent()
-    val btnShare: LiveData<Unit> = _btnShare
-
-    private val _btnBack: SingleLiveEvent<Unit> = SingleLiveEvent()
-    val btnBack: LiveData<Unit> = _btnBack
     private val _customTimerDuration: MutableLiveData<Long> = MutableLiveData(MIllIS_IN_FUTURE)
     private var oldTimeMills: Long = 0
     private val _time = MutableLiveData<String>("")
     val timerJob: Job = viewModelScope.launch(start = CoroutineStart.LAZY) {
         withContext(Dispatchers.IO) {
             oldTimeMills = System.currentTimeMillis()
-            while (_customTimerDuration.value!! > 0L) {
+            while (_customTimerDuration.value!! > ZERO_INTERVAL) {
                 val delayMills = System.currentTimeMillis() - oldTimeMills
                 if (delayMills == TICK_INTERVAL) {
                     _customTimerDuration.postValue(_customTimerDuration.value!! - delayMills)
@@ -56,21 +47,11 @@ class ShareCodeViewModel @Inject constructor() : BaseViewModel() {
     val time: LiveData<String> = _time
 
 
-
-    fun onClickCheckButton() {
-        _btnNext.call()
-    }
-
-    fun onClickShareButton() {
-        _btnShare.call()
-    }
-
-    fun onClickBackButton() {
-        _btnBack.call()
-    }
-
     companion object {
-        const val MIllIS_IN_FUTURE = 1800000L
-        const val TICK_INTERVAL = 1000L
+        private const val MIllIS_IN_FUTURE = 1800000L
+        private const val TICK_INTERVAL = 1000L
+        private const val MINUTE_UNIT = 60000L
+        private const val SECOND_UNIT = 1000L
+        private const val ZERO_INTERVAL = 0L
     }
 }
