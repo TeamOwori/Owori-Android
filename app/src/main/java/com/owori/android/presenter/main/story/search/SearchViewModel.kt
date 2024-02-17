366b4cff0b2c119da3827415de15f1a15bcc8b2e
민대인 <bamin0422@gmail.com>
2024-02-17 21:48:55 +0900

    feat: 검색화면 UI 구현 (4) - 검색화면 mock 데이터 추가 #49

--- app/src/main/java/com/owori/android/presenter/main/story/search/SearchViewModel.kt
+++ app/src/main/java/com/owori/android/presenter/main/story/search/SearchViewModel.kt
@@ -3,7 +3,9 @@ package com.owori.android.presenter.main.story.search
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.owori.android.core.BaseViewModel
+import com.owori.android.presenter.model.CommentData
 import com.owori.android.presenter.model.PostData
+import com.owori.android.presenter.model.PostPhotoData
 import com.owori.android.presenter.util.SingleLiveEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -26,6 +28,7 @@ class SearchViewModel @Inject constructor() : BaseViewModel() {
 
     init {
         fetchRecentResultSearchKeyword()
+        fetchSearchResult()
     }
 
     fun onClickFinishButton() {
@@ -62,6 +65,112 @@ class SearchViewModel @Inject constructor() : BaseViewModel() {
             "속초",
             "여행",
             "택배"
+        )
+    }
+
+    private fun fetchSearchResult() {
+        _searchResult.value = listOf(
+            PostData(
+                id = 0,
+                photoList = listOf(
+                    PostPhotoData(
+                        0,
+                        "https://mblogthumb-phinf.pstatic.net/MjAyMTA0MTRfMzEg/MDAxNjE4MzkzNzYyMTI3.2O_46TmKTYGbXMeu-W_9wdfAAw-tYLPzzRO2ZhB7hesg.iBmAqi17fck3tC4907gGaFIp1IalksYgIGCdjhhSuzAg.JPEG.eu2/1618393754909.jpg?type=w800"
+                    ),
+                    PostPhotoData(
+                        1, "https://cdn.imweb.me/upload/S20210809c06cc49e8b65a/21eaaf7839ec5.jpg"
+                    ),
+                ),
+                title = "무슨 영화세트장인줄..!",
+                contents = "엄마가 추천해줘서 간 연암지.. 풍경이 미쳤다.! 특히 해질 때 노을이랑 같이 보니까 극락온줄알았어요..",
+                startDate = "2023.03.12",
+                endDate = "2023.03.13",
+                likeCount = 4,
+                commentList = listOf(CommentData(0, "꺄르륵", "ㅎㅇㅎㅇㅎㅇ")),
+                author = "쥐렁이"
+            ),
+            PostData(
+                id = 0, photoList = listOf(
+                    PostPhotoData(
+                        1,
+                        "https://cdn.imweb.me/upload/S20210809c06cc49e8b65a/21eaaf7839ec5.jpg"
+                    ),
+                ), title = "무슨 영화세트장인줄..!",
+                contents = "엄마가 추천해줘서 간 연암지.. 풍경이 미쳤다.! 특히 해질 때 노을이랑 같이 보니까 극락온줄알았어요..",
+                startDate = "2023.03.13",
+                likeCount = 4,
+                commentList = listOf(CommentData(0, "꺄르륵", "ㅎㅇㅎㅇㅎㅇ")),
+                author = "쥐렁이"
+            ),
+            PostData(
+                id = 0, photoList = listOf(
+                    PostPhotoData(
+                        1,
+                        "https://cdn.imweb.me/upload/S20210809c06cc49e8b65a/21eaaf7839ec5.jpg"
+                    ),
+                ), title = "무슨 영화세트장인줄..!",
+                contents = "엄마가 추천해줘서 간 연암지.. 풍경이 미쳤다.! 특히 해질 때 노을이랑 같이 보니까 극락온줄알았어요..",
+                startDate = "2023.04.12",
+                likeCount = 4,
+                commentList = listOf(CommentData(0, "꺄르륵", "ㅎㅇㅎㅇㅎㅇ")),
+                author = "쥐렁이"
+            ),
+            PostData(
+                id = 0, photoList = listOf(
+                    PostPhotoData(
+                        1,
+                        "https://cdn.imweb.me/upload/S20210809c06cc49e8b65a/21eaaf7839ec5.jpg"
+                    ),
+                ), title = "무슨 영화세트장인줄..!",
+                contents = "엄마가 추천해줘서 간 연암지.. 풍경이 미쳤다.! 특히 해질 때 노을이랑 같이 보니까 극락온줄알았어요..",
+                startDate = "2023.04.12",
+                endDate = "2023.04.13",
+                likeCount = 4,
+                commentList = listOf(CommentData(0, "꺄르륵", "ㅎㅇㅎㅇㅎㅇ")),
+                author = "쥐렁이"
+            ),
+            PostData(
+                id = 0, photoList = listOf(
+                    PostPhotoData(
+                        1,
+                        "https://cdn.imweb.me/upload/S20210809c06cc49e8b65a/21eaaf7839ec5.jpg"
+                    ),
+                ), title = "무슨 영화세트장인줄..!",
+                contents = "엄마가 추천해줘서 간 연암지.. 풍경이 미쳤다.! 특히 해질 때 노을이랑 같이 보니까 극락온줄알았어요..",
+                startDate = "2023.04.12",
+                endDate = "2023.04.13",
+                likeCount = 4,
+                commentList = listOf(CommentData(0, "꺄르륵", "ㅎㅇㅎㅇㅎㅇ")),
+                author = "쥐렁이"
+            ),
+            PostData(
+                id = 0, photoList = listOf(
+                    PostPhotoData(
+                        1,
+                        "https://cdn.imweb.me/upload/S20210809c06cc49e8b65a/21eaaf7839ec5.jpg"
+                    ),
+                ), title = "무슨 영화세트장인줄..!",
+                contents = "엄마가 추천해줘서 간 연암지.. 풍경이 미쳤다.! 특히 해질 때 노을이랑 같이 보니까 극락온줄알았어요..",
+                startDate = "2023.04.12",
+                endDate = "2023.04.13",
+                likeCount = 4,
+                commentList = listOf(CommentData(0, "꺄르륵", "ㅎㅇㅎㅇㅎㅇ")),
+                author = "쥐렁이"
+            ),
+            PostData(
+                id = 0, photoList = listOf(
+                    PostPhotoData(
+                        1,
+                        "https://cdn.imweb.me/upload/S20210809c06cc49e8b65a/21eaaf7839ec5.jpg"
+                    ),
+                ), title = "무슨 영화세트장인줄..!",
+                contents = "엄마가 추천해줘서 간 연암지.. 풍경이 미쳤다.! 특히 해질 때 노을이랑 같이 보니까 극락온줄알았어요..",
+                startDate = "2023.04.12",
+                endDate = "2023.04.13",
+                likeCount = 4,
+                commentList = listOf(CommentData(0, "꺄르륵", "ㅎㅇㅎㅇㅎㅇ")),
+                author = "쥐렁이"
+            )
         )
     }
 }
